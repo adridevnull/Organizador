7bd7ec67518a8d38f1bc1dce2724068e5295622f
Adrián Almagro <sustodka@gmail.com>
2021-04-16 00:58:48 +0200

    push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CalendarioComponent } from './calendario/calendario.component';
+import { ChatComponent } from './chat/chat.component';
+import { ModulosComponent } from './modulos/modulos.component';
+import { ProfesoresComponent } from './profesores/profesores.component';
+
+const rutas = [
+  { path: 'calendario', component: CalendarioComponent },
+  { path: 'modulos', component: ModulosComponent },
+  { path: 'profesores', component: ProfesoresComponent }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatComponent
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(rutas),
     AppRoutingModule
   ],
   providers: [],
